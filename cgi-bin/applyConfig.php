6d3821102d1a1da6aa465aa0c32bf129a85cc171
Tobias Mädel <t.maedel@alfeld.de>
2021-02-18 19:45:25 +0100

    Add snapcast support for client & server

--- cgi-bin/applyConfig.php
+++ cgi-bin/applyConfig.php
@@ -1,6 +1,9 @@
 <?php
 require(__DIR__ . "/config.php");
 
+// 23 == 0dB capture gain
+`amixer cset name='Capture Volume' 23`;
+
 if ($config['CONFIG_AIRPLAY_ACTIVE'])
 {
 	`systemctl start shairport-sync`;
@@ -28,19 +31,47 @@ else
 	`systemctl stop librespot`;
 }
 
+if ($config['CONFIG_LINEIN_STREAM_ACTIVE'])
+{
+	`systemctl start snapserver`;
+}
+else
+{
+	`systemctl stop snapserver`;
+}
+
+if ($config['CONFIG_SNAPCAST_CLIENT_ACTIVE'])
+{
+	$snapclient_config = 'START_SNAPCLIENT=true' . "\n" .
+			     'SNAPCLIENT_OPTS="-h ' . escapeshellarg($config['CONFIG_SNAPCAST_CLIENT_HOST']) . '"' . "\n";
+	$snapclient_config_path = "/etc/default/snapclient";
+	if (file_get_contents($snapclient_config_path) != $snapclient_config)
+	{
+		file_put_contents($snapclient_config_path, $snapclient_config);
+		`systemctl restart snapclient`;
+	}
+	else
+	{
+		`systemctl start snapclient`;
+	}
+}
+else
+{
+	`systemctl stop snapclient`;
+}
 
 $wpaconf = 'ctrl_interface=/var/run/wpa_supplicant
 #ap_scan=1
 
 network={
-        ssid="'. $config['CONFIG_WIFI_SSID'] .'"
-        scan_ssid=1
-        key_mgmt=WPA-PSK
-        psk="'. $config['CONFIG_WIFI_PSK'] .'"
+		ssid="'. $config['CONFIG_WIFI_SSID'] .'"
+		scan_ssid=1
+		key_mgmt=WPA-PSK
+		psk="'. $config['CONFIG_WIFI_PSK'] .'"
 }
 ';
 
-$wpaconfPath = "/etc/wpa_supplicant/wpa_supplicant-wlan0.conf"; 
+$wpaconfPath = "/etc/wpa_supplicant/wpa_supplicant-wlan0.conf";
 if (file_get_contents($wpaconfPath) != $wpaconf)
 {
 	file_put_contents($wpaconfPath, $wpaconf);
@@ -71,6 +102,14 @@ if (trim(file_get_contents("/etc/hostname")) != trim($config['CONFIG_DISPLAY_NAM
 	if ($config['CONFIG_LIBRESPOT_ACTIVE'])
 	{
 		`systemctl restart librespot`;
+	}
+	if ($config['CONFIG_SNAPCAST_CLIENT_ACTIVE'])
+	{
+		`systemctl restart snapclient`;
+	}
+	if ($config['CONFIG_LINEIN_STREAM_ACTIVE'])
+	{
+		`systemctl restart snapserver`;
 	}
 
 	$restarted_services = true;
